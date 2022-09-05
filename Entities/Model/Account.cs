32beb1589909c91cbd22221dae8de8e90600dfb2
UBSHARMA <UBSHARMA@UBSHARMA>
2022-09-05 22:12:45 +0530

    Add project files.

---
+++ Entities/Model/Account.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Model
+{
+    [Table("account")]
+    public class Account
+    {
+        [Key]   
+        public Guid AccountId { get; set; }
+
+        [Required(ErrorMessage = "DateCreated is required.")]
+        public DateTime DateCreated { get; set; }
+
+        [Required(ErrorMessage = "Account Type is required.")]
+        public string AccountType { get; set; }
+
+        [ForeignKey(nameof(Owner))]
+        public Guid OwnerId { get; set; }
+        public Owner Owner { get; set; }
+    }
+}
---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,42 @@
+﻿using Contracts;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        public RepositoryContext repositoryContext { get; set; }
+
+        public RepositoryBase(RepositoryContext context)
+        {
+            repositoryContext = context;
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return repositoryContext.Set<T>().AsNoTracking();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> condition)
+        {
+            return repositoryContext.Set<T>().Where(condition).AsNoTracking();
+        }
+
+        public void Create(T entity)
+        {
+            repositoryContext.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            repositoryContext.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            repositoryContext.Set<T>().Remove(entity);
+        }
+    }
+}
